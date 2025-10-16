e1325e4fc2012a74aeb570811446ddefce0ba945
Diego <diegoamarilla478@gmail.com>
2025-10-16 09:01:08 -0300

    presentacion en cantidad asc

--- src/main/java/com/franco/dev/service/productos/PresentacionService.java
+++ src/main/java/com/franco/dev/service/productos/PresentacionService.java
@@ -1,58 +1,68 @@
 package com.franco.dev.service.productos;
 
 import com.franco.dev.domain.productos.Presentacion;
-import com.franco.dev.domain.productos.Producto;
 import com.franco.dev.repository.productos.PresentacionRepository;
-import com.franco.dev.repository.productos.ProductoRepository;
 import com.franco.dev.service.CrudService;
+import com.franco.dev.service.utils.ImageService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
-public class PresentacionService extends CrudService<Presentacion, PresentacionRepository> {
+public class PresentacionService extends CrudService<Presentacion, PresentacionRepository, Long> {
 
     private final PresentacionRepository repository;
-    private final ProductoRepository productoRepository;
+
+    @Autowired
+    private ImageService imageService;
 
     @Override
     public PresentacionRepository getRepository() {
         return repository;
     }
-
-    public List<Presentacion> findByAll(String texto) {
+    public List<Presentacion> findByAll(String texto){
         texto = texto.replace(' ', '%');
-        return repository.findByAll(texto.toUpperCase());
+        return  repository.findByAll(texto.toUpperCase())
+                .stream()
+                .sorted(Comparator.comparing(Presentacion::getCantidad))
+                .collect(Collectors.toList());
     }
-
-    public List<Presentacion> findByProductoId(Long id) {
-        return repository.findByProductoId(id);
+    public List<Presentacion> findByProductoIdOrderByCantidadAsc(Long id){
+        return repository.findByProductoIdOrderByCantidadAsc(id);
     }
-
-//    public Presentacion recibirPresentacion(Presentacion pCentral) {
-//        Presentacion foundPresentacion = repository.findByIdCentral(pCentral.getId());
-//        Producto producto = productoRepository.findByIdCentral(pCentral.getProducto().getId());
-//        if (producto != null) {
-//            if (foundPresentacion == null) {
-//                foundPresentacion = pCentral;
-//                foundPresentacion.setIdCentral(pCentral.getId());
-//                foundPresentacion.setId(null);
-//                foundPresentacion = save(foundPresentacion);
-//            }
-//        }
-//        return foundPresentacion;
-//    }
+    public List<Presentacion> findByProductoId(Long id){
+        return findByProductoIdOrderByCantidadAsc(id);
+    }
 
     @Override
-    public Presentacion save(Presentacion entity) {
+    public Presentacion save(Presentacion entity){
+        if(entity.getId()==null) entity.setCreadoEn(LocalDateTime.now());
         Presentacion e = super.save(entity);
 //        personaPublisher.publish(p);
+        repository.flush();
         return e;
     }
 
-    public Presentacion findByPrincipalAndProductoId(Boolean principal, Long id) {
+    public Presentacion findByPrincipalAndProductoId(Boolean principal, Long id){
         return repository.findByPrincipalAndProductoId(principal, id);
+    }
+
+    public void enviarImagenes(Long sucId){
+        List<Presentacion> presentacionList = findAll2();
+        for(Presentacion p: presentacionList){
+            String image = imageService.getImageWithMediaType(p.getId()+".jpg", imageService.getImagePresentaciones());
+            if(image!="") {
+                log.info("Imagen encontrada: " + imageService.getImagePresentaciones()+p.getId()+".jpg");
+//                propagacionService.propagarImagen(image, p.getId() + ".jpg", TipoEntidad.PRESENTACION, sucId);
+            } else {
+//                log.info("Imagen no encontrada: " + imageService.getImagePresentaciones()+p.getId()+".jpg");
+            }
+        }
     }
 }
