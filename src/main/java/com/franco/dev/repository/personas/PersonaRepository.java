289fd79d5e78c6405c3eece6f384b1a5a234ceb3
Diego <diegoamarilla478@gmail.com>
2026-03-17 15:46:55 -0300

    paginacion para lista de personas

--- src/main/java/com/franco/dev/repository/personas/PersonaRepository.java
+++ src/main/java/com/franco/dev/repository/personas/PersonaRepository.java
@@ -3,6 +3,8 @@ package com.franco.dev.repository.personas;
 import com.franco.dev.domain.personas.Persona;
 import com.franco.dev.repository.HelperRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -18,4 +20,7 @@ public interface PersonaRepository extends HelperRepository<Persona, Long> {
 
     @Query("select p from Persona p where CAST(id as text) like %?1% or UPPER(p.nombre) like %?1% or UPPER(p.apodo) like %?1% or UPPER(p.documento) like %?1%")
     public List<Persona> findbyAll(String texto);
+
+    @Query("select p from Persona p where CAST(id as text) like %?1% or UPPER(p.nombre) like %?1% or UPPER(p.apodo) like %?1% or UPPER(p.documento) like %?1%")
+    public Page<Persona> findbyAll(String texto, Pageable pageable);
 }
